7ad7b1e29614afca27bd54454eb88a3ee0631f29
siva <root@ip-172-31-31-86.ap-south-1.compute.internal>
2021-08-12 11:54:36 +0000

    months-choice

---
+++ months.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+case $n in
+        1)
+                echo "January" ;;
+        2)
+                echo "Feburary" ;;
+        3)
+                echo "March" ;;
+        4)
+                echo "April" ;;
+        5)
+                echo "May" ;;
+        6)
+                echo "June" ;;
+        7)
+                echo "July" ;;
+esac
+
